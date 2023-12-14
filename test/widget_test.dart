a55d0cbbbe306eee5d00fc24ef1f536f8f6dc11e
创圣大天使Evol <296662402@qq.com>
2023-12-14 16:52:32 +0800

    widget_test.dart - test the home page and tap the fab, then create a new task will pageback

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,10 +5,12 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_app/main.dart';
+import 'package:flutter_app/pages/projects/project.dart';
 import 'package:flutter_test/flutter_test.dart';
-
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
@@ -16,15 +18,44 @@ void main() {
     await tester.pumpWidget(MyApp());
 
     // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    {
+      expect(find.text('Today'), findsNWidgets(2));
+      expect(find.text('Inbox'), findsOneWidget);
+      expect(find.text('Next 7 Days'), findsOneWidget);
+      expect(find.byType(FloatingActionButton), findsOneWidget);
+    }
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
+    await tester.tap(find.byType(FloatingActionButton));
     await tester.pump();
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    {
+      expect(find.text('Add Task'), findsOneWidget);
+      expect(find.text('Project'), findsOneWidget);
+      expect(find.text('Due Date'), findsOneWidget);
+      expect(find.text('Priority'), findsOneWidget);
+      expect(find.text('Labels'), findsOneWidget);
+      expect(find.text('Comments'), findsOneWidget);
+      expect(find.text('Reminder'), findsOneWidget);
+      expect(find.byType(FloatingActionButton), findsOneWidget);
+      expect(find.byType(TextFormField), findsOneWidget);
+    }
+
+    await tester.enterText(find.byType(TextFormField), 'test');
+    await tester.pump();
+    await tester.tap(find.byType(FloatingActionButton));
+    await tester.pump();
+
+    {
+      expect(find.text('Add Task'), findsNothing);
+    }
+
+    //
+    // // Tap the '+' icon and trigger a frame.
+    // await tester.tap(find.byIcon(Icons.add));
+    // await tester.pump();
+    //
+    // // Verify that our counter has incremented.
+    // expect(find.text('0'), findsNothing);
+    // expect(find.text('1'), findsOneWidget);
   });
 }
