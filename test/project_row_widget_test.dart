ded335719ebec453844d90fdba10ac85bdac64d0
创圣大天使Evol <296662402@qq.com>
2023-12-17 16:24:51 +0800

    project_row_widget_test.dart - ProjectRowItem display() - after tap the ProjectRowItem, new page will be loaded

--- test/project_row_widget_test.dart
+++ test/project_row_widget_test.dart
@@ -43,40 +43,4 @@ main() {
     await tester.tap(find.byKey(ValueKey("tile_Personal_1")));
   });
 
-  testWidgets("Label Row Widget", (tester) async {
-    final homeBloc = HomeBloc();
-    var testLabel = Label.update(
-        id: 1,
-        name: "Android",
-        colorName: "Green",
-        colorCode: Colors.green.value);
-
-    await tester
-        .pumpWidget(LabelRow(testLabel).wrapScaffoldWithBloc(homeBloc));
-
-    expect(find.text("@ ${testLabel.name}"), findsOneWidget);
-
-    final iconLabel = tester.findWidgetByKey<Icon>("icon_Android_1");
-    expect(iconLabel.color!.value, testLabel.colorValue);
-
-    final spaceContainer = tester.findWidgetByKey<Container>("space_Android_1");
-    expect(spaceContainer.constraints!.widthConstraints().maxWidth, 24.0);
-    expect(spaceContainer.constraints!.heightConstraints().maxHeight, 24.0);
-  });
-
-  testWidgets("Label Row Tap", (tester) async {
-    final homeBloc = HomeBloc();
-    var testLabel = Label.update(
-        id: 1,
-        name: "Android",
-        colorName: "Green",
-        colorCode: Colors.green.value);
-
-    await tester
-        .pumpWidget(LabelRow(testLabel).wrapScaffoldWithBloc(homeBloc));
-
-    expect(homeBloc.title, emitsInOrder(["@ ${testLabel.name}"]));
-    expect(homeBloc.filter, emitsInOrder([Filter.byLabel(testLabel.name)]));
-    await tester.tap(find.byKey(ValueKey("tile_Android_1")));
-  });
 }
