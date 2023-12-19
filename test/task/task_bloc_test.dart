5e437cf48b8ed1cbc7a7dc7fe3b8a1db6c80380a
创圣大天使Evol <296662402@qq.com>
2023-12-19 23:18:24 +0800

    task_bloc_test.dart :: getTasks() fake implementations :: byProject & byLabel & default

--- test/task/task_bloc_test.dart
+++ test/task/task_bloc_test.dart
@@ -3,25 +3,58 @@ import 'package:test/test.dart';
 import 'package:flutter_app/pages/tasks/task_db.dart';
 import 'package:mockito/mockito.dart';
 
+import '../test_data.dart';
+
 class FakeTaskDb extends Fake implements TaskDB {
   List<Tasks> taskList = List.empty(growable: true);
 
   @override
   Future<List<Tasks>> getTasks(
       {int startDate = 0, int endDate = 0, TaskStatus? taskStatus}) async {
-    throw UnimplementedError();
+    if (!taskList.contains(testTask1)) {
+      taskList.add(testTask1);
+    }
+    if (!taskList.contains(testTask2)) {
+      taskList.add(testTask1);
+    }
+    if (!taskList.contains(testTask3)) {
+      taskList.add(testTask1);
+    }
+    return Future.value(taskList);
   }
 
   @override
   Future<List<Tasks>> getTasksByProject(int projectId,
       {TaskStatus? status}) async {
-    throw UnimplementedError();
+    if (!taskList.contains(testTask1)) {
+      taskList.add(testTask1);
+    }
+    if (!taskList.contains(testTask2)) {
+      taskList.add(testTask1);
+    }
+    if (!taskList.contains(testTask3)) {
+      taskList.add(testTask1);
+    }
+    taskList.removeWhere((element) => element.projectId != projectId);
+    return Future.value(taskList);
   }
 
   @override
   Future<List<Tasks>> getTasksByLabel(String labelName,
       {TaskStatus? status}) async {
-    throw UnimplementedError();
+    if (!taskList.contains(testTask1)) {
+      testTask1.labelList = ['Android'];
+      taskList.add(testTask1);
+    }
+    if (!taskList.contains(testTask2)) {
+      testTask2.labelList = ['Flutter'];
+      taskList.add(testTask2);
+    }
+    if (!taskList.contains(testTask3)) {
+      testTask3.labelList = ['React'];
+      taskList.add(testTask1);
+    }
+    return Future.value(taskList);
   }
 
   @override
